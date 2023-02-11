aec54098e403f2836bcc25c9e7ec2ec4e0287ab3
Dumble009 <daichi19990821@gmail.com>
2023-02-11 21:47:02 +0900

    main.cppにOpenALの機能を呼び出すコードを追加。
    問題無くビルド、実行することが出来た。

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,9 +1,36 @@
 ﻿// ----------STLのinclude----------
-#include <iostream> // 入出力
+#include <iostream>
+#include <string>
+
+// ----------OpenALのinclude-------
 #include "al.h"
 #include "alc.h"
 
 int main(void)
 {
-    std::cout << "Hello World !!" << std::endl;
+    std::cout << "Start!!" << std::endl;
+
+    auto Device = alcOpenDevice(NULL);
+
+    if (Device)
+    {
+        auto Context = alcCreateContext(Device, NULL);
+    }
+
+    auto defaultDeviceSpecifier = alcGetString(NULL, ALC_DEFAULT_DEVICE_SPECIFIER);
+    std::string deviceName = std::string(defaultDeviceSpecifier);
+    std::cout << "Device Name is : " << deviceName << std::endl;
+
+    auto g_bEAX = alIsExtensionPresent("EAX2.0");
+
+    alGetError();
+
+    auto Context = alcGetCurrentContext();
+    Device = alcGetContextsDevice(Context);
+    alcMakeContextCurrent(NULL);
+    alcDestroyContext(Context);
+    alcCloseDevice(Device);
+
+    std::cout << "Finish!!" << std::endl;
+    return 0;
 }
